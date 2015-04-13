c0e0b7c2cfa2566d063710f24b196ee66bf1caf4
Dennis Nygren <dennis@pxlbros.com>
2015-04-13 16:59:13 -0700

    Added some content to CSS/JS HTML functions.

--- src/resources/views/layouts/partials/css.php
+++ src/resources/views/layouts/partials/css.php
@@ -1,1 +1,3 @@
-<?php
+<?php foreach ( $assets[\PXLBros\PXLBase\Controllers\CoreController::ASSET_CSS] as $css_file ): ?>
+	<link href="<?= ($css_file['external'] === FALSE ? $base_url : '') . $css_file['path'] ?>" rel="stylesheet">
+<?php endforeach ?>
--- src/resources/views/layouts/partials/js.php
+++ src/resources/views/layouts/partials/js.php
@@ -1,1 +1,5 @@
-<?php
+<?= $jquery . $inline_js ?>
+
+<?php foreach ( $assets[\PXLBros\PXLBase\Controllers\CoreController::ASSET_JS] as $js_file ): ?>
+	<script src="<?= ($js_file['external'] === FALSE ? $base_url : '') . $js_file['path'] ?>"></script>
+<?php endforeach ?>
