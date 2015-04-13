d7fb4e11f15e1ff3950424fcc952ba112c71da35
Dennis Nygren <dennis@pxlbros.com>
2015-04-13 15:38:04 -0700

    Various changes.

--- src/PXLBros/pxlBase/PXLBaseServiceProvider.php
+++ src/PXLBros/pxlBase/PXLBaseServiceProvider.php
@@ -6,9 +6,32 @@ class PXLBaseServiceProvider extends \Illuminate\Support\ServiceProvider
 
 	public function boot()
 	{
+		$this->publishes(
+		[
+			__DIR__ . '/../../config/pxlbase.php' => config_path('pxlbase.php')
+		]);
 	}
 
 	public function register()
 	{
+		$this->mergeConfigFrom
+		(
+			__DIR__ . '/../../config/pxlbase.php', 'pxlbase'
+		);
+
+		$this->registerServices();
+	}
+
+	public function provides()
+	{
+		return ['pxlbase'];
+	}
+
+	protected function registerServices()
+	{
+		$this->app->booting(function ($app)
+		{
+			$app['modules']->register();
+		});
 	}
 }
