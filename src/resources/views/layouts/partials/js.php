a72c7ed3df145cde06ba0cfecefffcb1e9bb4656
Dennis Nygren <dennis@pxlbros.com>
2015-04-13 18:22:53 -0700

    Fixed JS file.

--- src/resources/views/layouts/partials/js.php
+++ src/resources/views/layouts/partials/js.php
@@ -1,5 +1,3 @@
-<?= $jquery ?>
-
 <?php foreach ( $js_files as $js_file ): ?>
 	<script src="<?= ($js_file['external'] === FALSE ? $base_url : '') . $js_file['path'] ?>"></script>
 <?php endforeach ?>
