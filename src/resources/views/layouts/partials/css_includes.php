365a87930a903007669ee1d2818303e5fe7a7553
DennisNygren <dennis@pxlbros.com>
2015-04-29 22:03:25 -0700

    Delayed CSS/JS functionality.

--- src/resources/views/layouts/partials/css_includes.php
+++ src/resources/views/layouts/partials/css_includes.php
@@ -1,3 +1,11 @@
 <?php foreach ( $css_files as $css_file ): ?>
-	<link href="<?= ($css_file['external'] === FALSE ? $base_url : '') . $css_file['path'] ?>" rel="stylesheet">
+	<?php if ( $css_file['delayed'] === false ): ?>
+		<link href="<?= ($css_file['external'] === FALSE ? $base_url : '') . $css_file['path'] ?>" rel="stylesheet">
+	<?php endif ?>
+<?php endforeach ?>
+
+<?php foreach ( $css_files as $css_file ): ?>
+	<?php if ( $css_file['delayed'] === true ): ?>
+		<link href="<?= ($css_file['external'] === FALSE ? $base_url : '') . $css_file['path'] ?>" rel="stylesheet">
+	<?php endif ?>
 <?php endforeach ?>
